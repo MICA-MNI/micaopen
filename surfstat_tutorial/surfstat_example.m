1706e3aa70fc6023c9a3f6f8dd4464dce52d0768
Neurobiomarkers <boris.bernhardt@gmail.com>
2017-05-03 14:03:44 -0400

    surfstat_tutorial v1

---
+++ surfstat_tutorial/surfstat_example.m
@@ -0,0 +1,11 @@
+%% CHANGE THIS FOR YOUR LOCAL PATH
+P='http://mica-mni.github.io/micaopen/surfstat_tutorial/'; 
+
+%% get this tool from mica-mni.github.io/micaopen/surfstat_tutorial/
+addpath([P 'surfstat'])
+
+%% Load the surface data 
+SW = SurfStatAvSurface({''})
+
+
+
