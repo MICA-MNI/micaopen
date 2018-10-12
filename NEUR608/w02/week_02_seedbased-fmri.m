d6cae036ff32eb9e7e703ab5370b38a202aab477
Boris Bernhardt <boris.bernhardt@gmail.com>
2018-10-12 06:12:11 -0400

    update examples

--- NEUR608/w02/week_02_seedbased-fmri.m
+++ NEUR608/w02/week_02_seedbased-fmri.m
@@ -1,31 +1,38 @@
 %% --
 % load this github from 
+clear 
 GH = '/Users/boris/Documents/1_github/micaopen'
 addpath(genpath(GH))
 
 myFile = 'ts.mat';
 mySurf = 'SM.mat';
 myYeo  = 'yeo.mat'; 
+myLabel = 'label-Cambridge308.fsa5-LR.mat'
 
 load(mySurf); 
 load(myFile);
 load(myYeo);
+load(myLabel); 
+
+
+f=figure, BoSurfStatViewData(label_fsa5, SM,'')
+parcelts = mica_surfData2parcelData(ts, label_fsa5); 
+
+
 
 %% --   
-% build correlations 
-r               = corr(ts);
+% build correlation matrix 
+r               = corr(parcelts);
 z               = 0.5 * log( (1+r) ./ (1-r) ); 
 z(isinf(z))     = 0; 
 z(isnan(z))     = 0;
-mPFC            = 730; 
-PCC             = 3215; 
-VIS             = 2748; 
-
-f=figure;  
-    SurfStatViewData(z(1880,:).*(yeo>0),SM,'','black');
-    SurfStatColLim([.2 0.7]);
-    colormap([.8 .8 .8 ; hot]);
     
+f=figure, 
+    imagesc(z,[0 1])
+    
+ 
+
+% build affinity matrix 
 
     
  
