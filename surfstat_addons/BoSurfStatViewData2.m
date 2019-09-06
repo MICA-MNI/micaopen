eb7ec8218745f9cdbd0b8543a24bb22d2590bc59
Boris Bernhardt <boris.bernhardt@gmail.com>
2019-09-06 09:36:05 -0400

    NEUR608 MATERIAL

--- surfstat_addons/BoSurfStatViewData2.m
+++ surfstat_addons/BoSurfStatViewData2.m
@@ -1,4 +1,8 @@
+<<<<<<< Updated upstream
 function [a,cb]=BoSurfStatViewData2(data,surf,title,background,clim);
+=======
+function [a,cb]=BoSurfStatViewData(data,surf,title,background);
+>>>>>>> Stashed changes
 
 %BoSurfStatViewData is a simple viewer for surface data.
 % 
@@ -28,6 +32,7 @@ vr=vl+v/2;
 t=size(surf.tri,1);
 tl=1:(t/2);
 tr=tl+t/2;
+<<<<<<< Updated upstream
 if nargin<5
     
     clim=[min(data),max(data)];
@@ -36,35 +41,61 @@ if nargin<5
         clim=clim(1)+[-1 0];
     end
 end
+=======
+clim=[min(data),max(data)];
+if clim(1)==clim(2)
+    clim=clim(1)+[-1 0];
+end
+
+>>>>>>> Stashed changes
 clf;
 colormap(spectral(256));
 
 
+<<<<<<< Updated upstream
 h=0.25;
 w=0.20;
 
 a(1)=axes('position',[0.3 0.6 w h]);
+=======
+h=0.27;
+w=0.23;
+
+a(1)=axes('position',[0.26 0.55 w h]);
+>>>>>>> Stashed changes
 trisurf(surf.tri(tl,:),surf.coord(1,vl),surf.coord(2,vl),surf.coord(3,vl),...
     double(data(vl)),'EdgeColor','none');
 view(-90,0); 
 daspect([1 1 1]); axis tight; camlight; axis vis3d off;
 lighting phong; material dull; shading interp;
 
+<<<<<<< Updated upstream
 a(2)=axes('position',[0.3+w 0.6 w h]);
+=======
+a(2)=axes('position',[0.26+w 0.55 w h]);
+>>>>>>> Stashed changes
 trisurf(surf.tri(tl,:),surf.coord(1,vl),surf.coord(2,vl),surf.coord(3,vl),...
     double(data(vl)),'EdgeColor','none');
 view(90,0); 
 daspect([1 1 1]); axis tight; camlight; axis vis3d off;
 lighting phong; material dull; shading interp;
 
+<<<<<<< Updated upstream
 a(3)=axes('position',[0.3 0.3 w h]);
+=======
+a(3)=axes('position',[0.26 0.33 w h]);
+>>>>>>> Stashed changes
 trisurf(surf.tri(tr,:)-v/2,surf.coord(1,vr),surf.coord(2,vr),surf.coord(3,vr),...
     double(data(vr)),'EdgeColor','none');
 view(-90,0); 
 daspect([1 1 1]); axis tight; camlight; axis vis3d off;
 lighting phong; material dull; shading interp;
 
+<<<<<<< Updated upstream
 a(4)=axes('position',[0.3+w 0.3 w h]);
+=======
+a(4)=axes('position',[0.26+w 0.33 w h]);
+>>>>>>> Stashed changes
 trisurf(surf.tri(tr,:)-v/2,surf.coord(1,vr),surf.coord(2,vr),surf.coord(3,vr),...
     double(data(vr)),'EdgeColor','none');
 view(90,0); 
@@ -77,9 +108,14 @@ for i=1:length(a)
     set(a(i),'Tag',['SurfStatView ' num2str(i) ]);
 end
 
+<<<<<<< Updated upstream
 
 cb=colorbar('location','South');
 set(cb,'Position',[0.35 0.22 0.3 0.03]);
+=======
+cb=colorbar('location','East');
+set(cb,'Position',[0.8 0.47 0.05 0.2]);
+>>>>>>> Stashed changes
 set(cb,'XAxisLocation','bottom');
 h=get(cb,'Title');
 set(h,'String',title);
